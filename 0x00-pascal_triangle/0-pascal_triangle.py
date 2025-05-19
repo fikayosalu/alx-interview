3cf3072d6d105c87a5b8ecdea8b64714f98c0116
fikayosalu <salufikayo@gmail.com>
2025-05-19 18:38:26 +0100

    Completed Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" 0-pascal_triangle.py """
+
+
+def pascal_triangle(n):
+    """ returns a list of lists of integers representing
+    the Pascal’s triangle of n """
+    triangle = [[1]]
+
+    if n <= 0:
+        return []
+
+    for i in range(n - 1):
+        prev = triangle[-1]
+        row = [1]
+
+        for j in range(len(prev) - 1):
+            row.append(prev[j] + prev[j + 1])
+        row.append(1)
+        triangle.append(row)
+    return triangle
