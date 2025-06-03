8b50d36ec7431e6116ed59a4949b30c15aaaee5b
fikayosalu <salufikayo@gmail.com>
2025-06-03 12:28:15 +0100

    Done task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""  0-lockboxes """
+
+def canUnlockAll(boxes):
+    """
+    Checks if all boxes can be opened starting from box 0.
+
+    Returns:
+        bool: True if all boxes can be opened, else False.
+    """
+    n = len(boxes)            # Total number of boxes
+    visited = set()           # Set to keep track of opened boxes
+    stack = [0]               # Start with box 0 (already unlocked)
+
+    while stack:
+        current = stack.pop()
+        if current not in visited:
+            visited.add(current)
+            for key in boxes[current]:
+                if 0 <= key < n and key not in visited:
+                    stack.append(key)
+
+    return len(visited) == n
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,3 @@
+You have n number of locked boxes in front of you. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+
+This directory contains a module with a method that determines if all the boxes can be opened.
